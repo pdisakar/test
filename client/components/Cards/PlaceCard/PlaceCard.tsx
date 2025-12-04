cd264874e75383a60fe8c10694d15b53690c8a16
Sakar Pudasaini <pdi.sakar@gmail.com>
2025-12-04 12:07:33 +0545

    working on places

--- client/components/Cards/PlaceCard/PlaceCard.tsx
+++ client/components/Cards/PlaceCard/PlaceCard.tsx
@@ -9,18 +9,30 @@ interface PlaceCardProps {
 }
 
 export const PlaceCard: React.FC<PlaceCardProps> = ({ data }) => {
+    console.log(data);
+
     return (
-
-
-        <Link href={`/${data.slug}`}>
-            <figure className='image-slot aspect-320/420'>
+        <Link href={`/${data.slug}`} className='relative rounded-lg group'>
+            <figure className='image-slot aspect-320/420 rounded-lg'>
                 <Image
                     src={data.featuredImage ? `${IMAGE_URL}${data.featuredImage}` : '/placeholder.jpg'}
                     alt={data.featuredImageAlt || data.title}
                     fill
                     priority
+                    className='rounded-lg'
                 />
             </figure>
+            <figcaption className=' absolute left-0 right-0 bottom-0 z-10 text-white px-6 py-4 bg-linear-to-t from-black/70 to-transparent rounded-b-lg'>
+                <h2 className="text-[22px] font-semibold transform transition-all text-shadow-md duration-300 translate-y-4 opacity-100 group-hover:-translate-y-2">
+                    {data.title}
+                </h2>
+                <p className="text-sm font-light transform transition-all text-shadow-sm duration-300 translate-y-4 opacity-0 group-hover:-translate-y-2 group-hover:opacity-100">
+                    {String(data.packageCount).padStart(2, "0")} Packages
+                </p>
+
+
+
+            </figcaption>
         </Link>
 
     );
